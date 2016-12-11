33e067eb59ae9ed77336cc8b741b623fbe977dbb
Daniel Hug <dhug@albany.edu>
2016-12-11 17:23:40 -0500

    Added the inOrder traversal class for printing out binary trees inorder

--- Assignment6/src/BinaryTree.java
+++ Assignment6/src/BinaryTree.java
@@ -1,9 +1,48 @@
-
+/**
+ * Daniel Hug
+ * CSI 213
+ * Binary Tree
+ *
+ */
 public class BinaryTree {
 
-	private class Node{
-		
+	 class Node{
+		String value;
+		Node left, right;
+		/**
+		 * Constructor for Node class
+		 * @param val value of the Node
+		 */
+		Node(String val){
+			value = val;
+			left = null;
+			right = null;
+					
+		}
+		/**
+		 * Second Constructor
+		 * @param val value of the Node
+		 * @param left1 The Node to the left of val
+		 * @param right1 The Node to the right of val
+		 */
+		Node(String val, Node left1, Node right1){
+			value = val;
+			left = left1;
+			right = right1;
+		}
 	}
+	 
+	 public void inOrder(Node bTree){
+		//If empty do nothing
+		 if(bTree != null){
+			 //Traverse the left subtree
+			 inOrder(bTree.left);
+			 //Visit the Node
+			 System.out.println(bTree.value + " ");
+			 //Traverse the right subtree
+			 inOrder(bTree.right);
+		 }
+	 }
 	
 	public static void search(){
 		
